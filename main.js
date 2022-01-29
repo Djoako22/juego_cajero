630950f7c7a44547cd1861176527a9898696abb2
Joaquin Garcia <57043889+Djoako22@users.noreply.github.com>
2022-01-29 20:57:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ const dinero = document.querySelector(".dinero");
 const cambio = document.querySelector(".cambio");
 const cajaBilletes = document.querySelector(".cajaBilletes");
 const billete = document.querySelector(".billete");
+const temporizador = document.querySelector("temporizador");
+
+let t = 30;
+let cuantosClientes = 0;
 
 let clientes = ["👩", "👨", "👨‍⚕️", "👮‍♂️", "👩‍🎓", "👩‍🔬", "👨‍🍳"];
 let productos = [
@@ -30,12 +34,31 @@ let productos = [
     "👠",
 ];
 let precios = [50, 70, 100, 150, 200, 300, 500, 900];
-let billetes = [10, 20, 50, 100, 200, 500, 1000];
+let billetes = [10, 50, 100, 500];
+let billetes_cliente = [10, 20, 50, 100, 500, 1000];
 
 caja.addEventListener("submit", atender);
 
 function obtenerAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function tiempo() {
+    if (cuantosClientes > 0 && cuantosClientes % 5 == 0) {
+        t -= 2;
+    }
+    temporizador.innerHTML = t;
+    const x = setInterval(() => {
+        if (parseInt(temporizador.innerHTML) < 2) {
+            respuesta.innerHTML =
+                "<persona>😒</persona><texto>Ya fue, me voy a otro lado.</texto>";
+            clearInterval(x);
+            setTimeout(() => {
+                renderLocal();
+            }, 2000);
+        }
+        temporizador.innerHTML = parseInt(temporizador.innerHTML) - 1;
+    }, 1000);
 }
 
 function atender(e) {
@@ -46,6 +69,9 @@ function atender(e) {
     ) {
         sonido_correcto.play();
         respuesta.innerHTML = "<persona>👍</persona><texto>Gracias!</texto>";
+        // if (parseInt(temporizador.innerHTML)%10==0) {
+        // }
+        cuantosClientes++;
         setTimeout(() => {
             renderLocal();
         }, 1000);
@@ -71,18 +97,17 @@ function pagar(b) {
 
 function ayudar() {
     const r = obtenerAleatorio(0, 1);
-    if (r) {
-        respuesta.innerHTML = `<persona>😎</persona><texto>Te ayudo con el cambio, toma $${
-            100 - (parseInt(cambio.value) % 100)
-        }.</texto>`;
+    const ayuda = 100 - (parseInt(cambio.value) % 100);
+    if (r && parseInt(precio.value) != ayuda) {
+        respuesta.innerHTML = `<persona>😎</persona><texto>Te ayudo con el cambio, toma $${ayuda}.</texto>`;
         setTimeout(() => {
-            dinero.value =
-                parseInt(dinero.value) + 100 - (parseInt(cambio.value) % 100);
+            dinero.value = parseInt(dinero.value) + ayuda;
             cambio.value = 0;
         }, 1500);
     } else {
         sonido_correcto.play();
         respuesta.innerHTML = "<persona>👍</persona><texto>Gracias!</texto>";
+        cuantosClientes++;
         setTimeout(() => {
             renderLocal();
         }, 1000);
@@ -96,13 +121,14 @@ function renderLocal() {
         productos[obtenerAleatorio(0, productos.length - 1)]
     }</persona></texto>`;
     precio.value = precios[obtenerAleatorio(0, precios.length - 1)];
-    const queBilletes = billetes.filter((x) => x > precio.value);
+    const queBilletes = billetes_cliente.filter((x) => x > precio.value);
     dinero.value = queBilletes[obtenerAleatorio(0, queBilletes.length - 1)];
     cajaBilletes.innerHTML = "";
     billetes.forEach((b) => {
         cajaBilletes.innerHTML += `<button class="billete" onclick="pagar(${b})"><c>$${b}</c></button>`;
     });
     cambio.value = 0;
+    tiempo();
 }
 
 renderLocal();
